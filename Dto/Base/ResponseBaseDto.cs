5df231aaeb3b4caf4ef26f451441318bd52de859
mango <niesa1491@gmail.com>
2025-10-19 09:33:59 +0800

    Dto

---
+++ Dto/Base/ResponseBaseDto.cs
@@ -0,0 +1,42 @@
+﻿namespace LearnGamify.ResponseBaseDto
+{
+    public class Responser
+    {
+        public bool result { get; set; } = true;
+        public int statusCode { get; set; } = 200;
+        public string? methodName { get; set; }
+        public string? msg { get; set; }
+    }
+
+    public class ResponserT<T>
+    {
+        public bool result { get; set; } = true;
+        public int statusCode { get; set; } = 200;
+        public string? msg { get; set; }
+
+        public T? data { get; set; }
+    }
+
+
+    //public class ResponserPageT<T> : ResponserT<T>
+    //{
+    //    /// <summary>總筆數 預設:0</summary>
+    //    public int total { get; set; } = 0;
+    //}
+
+    //public class Responser_OperatorInfo
+    //{
+    //    public string? creator { get; set; }
+    //    public string? createTime { get; set; }
+    //    public string? updater { get; set; }
+    //    public string? updateTime { get; set; }
+    //    public string? remover { get; set; }
+    //    public string? removeTime { get; set; }
+    //}
+
+    //public class Response_Base_AccessToken
+    //{
+    //    public string? memberToken { get; set; }
+    //    public string? refreshToken { get; set; }
+    //}
+}
---
+++ Dto/MemberDto.cs
@@ -0,0 +1,46 @@
+﻿namespace LearnGamify.MemberDto
+{
+    public class RequestDto
+    {
+        public class Create
+        {
+            public string name { get; set; }
+        }
+
+        public class ReadList
+        {
+            public long? id { get; set; }
+        }
+
+        public class GetAuth
+        {
+            public string id { get; set; }
+            public string role { get; set; }
+        }
+
+        public class Update
+        {
+            public long id { get; set; }
+            public string name { get; set; }
+        }
+
+
+        public class Delete
+        {
+            public long id { get; set; }
+        }
+
+    }
+
+    public class ResponseDto
+    {
+        public class ReadList
+        {
+            public long id { get; set; }
+            public string? name { get; set; }
+        }
+
+
+    }
+
+}
